14d37efa9e214b7afe30b496a3d54aa2b172685e
João Santos <joao.santos@bway.pt>
2021-04-04 10:37:57 +0100

    add try catch to all async functions

--- catcher.js
+++ catcher.js
@@ -43,40 +43,63 @@ async function watchPairs() {
     for (let i = 0; i <= pairs.length - 1; i++) {  
         if (pairs[i].totalUSD > 10) {
             
-            let candlesticks = await binance.candlesticks(pairs[i].symbol, timeFrame, false, {limit: 1});
-            let percentToGo = Math.round((1 - pairs[i].value/candlesticks[0][3]) * 100 * 10) / 10;
-            let triggerQuote = Math.round(pairs[i].value * 1.007 * 1000000)/1000000;
+            try {
+                let candlesticks = await binance.candlesticks(pairs[i].symbol, timeFrame, false, {limit: 1});
 
-            if (percentToGo <= 5 && percentToGo > 2) {
+                let percentToGo = Math.round((1 - pairs[i].value/candlesticks[0][3]) * 100 * 10) / 10;
+                let triggerQuote = Math.round(pairs[i].value * 1.007 * 1000000)/1000000;
+
+                if (percentToGo <= 5 && percentToGo > 2) {
                 console.log('\x1b[0m', pairs[i].symbol, candlesticks[0][3], pairs[i].value, triggerQuote, percentToGo + '%');
-            }else if (percentToGo <= 2) {
-                console.log('\x1b[1m', pairs[i].symbol, candlesticks[0][3], pairs[i].value, triggerQuote, percentToGo + '%', '\x1b[0m');
-            }else if (percentToGo > 5) {
-                console.log('\x1b[2m', pairs[i].symbol, candlesticks[0][3], pairs[i].value, triggerQuote, percentToGo + '%', '\x1b[0m');
-            }
-        
-        
-            if (pairs[i].value * 1.007 >= candlesticks[0][3]) {
-                let buyQTY = Math.floor((pairs[i].totalUSD / pairs[i].value) * 10) / 10;
-                const activeOrders = await binance.openOrders(pairs[i].symbol);
-
-                if (activeOrders.length == 0 && !lockedPairs.includes(pairs[i].symbol)) {
-                    console.log('Adding buy order for ' + buyQTY + ' of ' + pairs[i].symbol);
-                    const buyOrder = await binance.buy(pairs[i].symbol, buyQTY, pairs[i].value);
-                    console.log(buyOrder);
-
-                    // LOCK THIS PAIR TO AVOID NEW ORDER (this lockage is still valid while the script is running as it is stored in an array)
-                    lockedPairs.push(pairs[i].symbol);
+                }else if (percentToGo <= 2) {
+                    console.log('\x1b[1m', pairs[i].symbol, candlesticks[0][3], pairs[i].value, triggerQuote, percentToGo + '%', '\x1b[0m');
+                }else if (percentToGo > 5) {
+                    console.log('\x1b[2m', pairs[i].symbol, candlesticks[0][3], pairs[i].value, triggerQuote, percentToGo + '%', '\x1b[0m');
                 }
-            }else {
-                const activeOrders = await binance.openOrders(pairs[i].symbol);
-                if (activeOrders.length > 0) {
-                    console.log('removing old order for ' + pairs[i].symbol);
-                    const cancelation = await binance.cancelAll(pairs[i].symbol);
 
-                    // AS THE ORDER IS CANCELED LET's FREE THIS SYMBOL FOR NEW ORDERS
-                    removeLockedPair(pairs[i].symbol);
+                if (pairs[i].value * 1.007 >= candlesticks[0][3]) {
+                    let buyQTY = Math.floor((pairs[i].totalUSD / pairs[i].value) * 10) / 10;
+                    try {
+                        const activeOrders = await binance.openOrders(pairs[i].symbol);
+
+                        if (activeOrders.length == 0 && !lockedPairs.includes(pairs[i].symbol)) {
+                            console.log('Adding buy order for ' + buyQTY + ' of ' + pairs[i].symbol);
+                            try {
+                                const buyOrder = await binance.buy(pairs[i].symbol, buyQTY, pairs[i].value);
+                            } catch (err) {
+                                console.log('buyOrder: ', err);
+                            }
+                            console.log(buyOrder);
+
+                            // LOCK THIS PAIR TO AVOID NEW ORDER (this lockage is still valid while the script is running as it is stored in an array)
+                            lockedPairs.push(pairs[i].symbol);
+                        }
+                    } catch (err) {
+                        console.log('activeOrders: ', err);
+                    }
+                    
+                }else {
+                    try {
+                        const activeOrders = await binance.openOrders(pairs[i].symbol);
+
+                        if (activeOrders.length > 0) {
+                            console.log('removing old order for ' + pairs[i].symbol);
+                            try {
+                                const cancelation = await binance.cancelAll(pairs[i].symbol);
+
+                                // AS THE ORDER IS CANCELED LET's FREE THIS SYMBOL FOR NEW ORDERS
+                                removeLockedPair(pairs[i].symbol);
+                            } catch (err) {
+                                console.log('cancelation: ', err);
+                            }
+                        }
+                    } catch (err) {
+                        console.log('activeOrders: ', err);
+                    }
                 }
+
+            } catch (err) {
+                console.log('candlesticks: ', err);
             }
         }
     }
